5734c7f393787bd6adc2c0f38cac5648d86fd9ca
Thuy Nguyen <57820377+tnguyen-tx@users.noreply.github.com>
2022-01-02 11:20:54 -0600

    Update SignExtender.v

--- SignExtender.v
+++ SignExtender.v
@@ -8,27 +8,27 @@ module SignExtender(BusImm, Imm32);
   begin  
 	if (Imm32[31:24] == 8'b10110100 || Imm32[31:24] == 8'b10110101) //CB-type
 		begin
-		  extBit <= Imm32[23]; 
-		   BusImm <= {{45{extBit}}, Imm32[23:5]};
+		  extBit = Imm32[23]; 
+		   BusImm = {{45{extBit}}, Imm32[23:5]};
 		end  
 	else if (Imm32[31:26] == 6'b000101 || Imm32[31:26] == 6'b100101) //B-type
 		begin
-		   extBit <= Imm32[25]; 
-		   BusImm <= {{38{extBit}}, Imm32[25:0]};
+		   extBit = Imm32[25]; 
+		   BusImm = {{38{extBit}}, Imm32[25:0]};
 		end 
 	else if (Imm32[31:21]==11'b11111000000 || Imm32[31:21] == 11'b11111000010) //D-type
 		begin
-		   extBit <= Imm32[20]; 
-		   BusImm <= {{55{extBit}}, Imm32[20:12]};
+		   extBit = Imm32[20]; 
+		   BusImm = {{55{extBit}}, Imm32[20:12]};
 		end 
 	else if (Imm32[31:21]== 11'b11010011011) 	//R-type for LSL
 		begin
-		   BusImm <= {26'b0, Imm32[15:10]};
+		   BusImm = {26'b0, Imm32[15:10]};
 		end
 	else if (Imm32[31:22] == 10'b??)
 		begin
-		   extBit <= Imm32[21]; 
-		   BusImm <= {{53{extBit}}, Imm32[21:10]};
+		   extBit = Imm32[21]; 
+		   BusImm = {{53{extBit}}, Imm32[21:10]};
 		end
 	else
 	BusImm=32'b0;
